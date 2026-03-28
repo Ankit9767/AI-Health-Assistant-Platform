6cd5011ff6de805596007d8c56e1b40c4d98f2a2
Ankit9767 <anktmb05@gmail.com>
2026-03-28 17:18:30 +0530

    added project into repo

---
+++ ai-health-assistant/src/main/java/ankit/ai_health_assistant/AiHealthAssistantApplication.java
@@ -0,0 +1,13 @@
+package ankit.ai_health_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHealthAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHealthAssistantApplication.class, args);
+	}
+
+}
---
+++ ai-health-assistant/src/test/java/ankit/ai_health_assistant/AiHealthAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ankit.ai_health_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHealthAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
